baa3d0c54da81e49e9b96033fa4dcef590fee464
tailwindTactician <>
2025-08-09 15:45:41 +0500

    commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿//Task 1
+//int cnt = 1;
+//
+//void Sum(int n)
+//{
+//    if (cnt > n) return;
+//
+//    Console.Write(cnt + " ");
+//    cnt++;
+//    Sum(n);
+//}
+
+//Task 2
+//int cnt = 0;
+//
+//void Sum(int n)
+//
+//{
+//    if (cnt < 1) return;
+//
+//    Console.Write(cnt + " ");
+//    cnt--;
+//    Sum(n);
+//}
+//
+//int n = Convert.ToInt32(Console.ReadLine());
+//cnt = n;
+// Sum(n);
+//Task 4
+//void Sum(int a)
+//{
+//    if (a == 0) return;
+//    Sum(a / 10); 
+//    Console.Write((a % 10) + " "); 
+//}
+//int a = Convert.ToInt32(Console.ReadLine());
+//Sum(a);
+//Task 3
+//int Sum(int a)
+//{
+//    if (a == 1) return 1;
+//    return a + Sum(a - 1);
+//}
+//
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(Sum(a));
+//Task 5
+//int Sum(int a)
+//{
+//    if (a == 0) return 0;
+//    return 1 + Sum(a / 10);
+//}
+//
+//int a = Convert.ToInt32(Console.ReadLine());
+//if (a == 0)
+//    Console.WriteLine(1);
+//else
+//    Console.WriteLine(Sum(Math.Abs(a)));
+//Task 6
+
